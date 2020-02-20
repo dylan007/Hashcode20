2d5310ee7b4ffe3eae32dfc1f23ca3f95e325025
Dheeraj <dheeraj98reddy@gmail.com>
2020-02-21 00:59:17 +0530

    Update .gitignore

---
+++ run.sh
@@ -0,0 +1,6 @@
+./a.out < a_example.txt > output_a
+./a.out < b_read_on.txt > output_b
+./a.out < c_incunabula.txt > output_c
+./a.out < d_tough_choices.txt > output_d
+./a.out < e_so_many_books.txt > output_e
+./a.out < f_libraries_of_the_world.txt > output_f
